6a0c3b32e6d52b6ec14ddb481bba7d4d04e9d907
Sophia Peraza <sophiasperaza@gmail.com>
2021-12-14 18:06:09 -0500

    Initial commit
    retrying

---
+++ README.md
@@ -0,0 +1,1 @@
+AHI-Sophia-Streamlit
---
+++ Streamlit_attempt.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 14 18:21:16 2021
+
+@author: sophi
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Medicare — Expenses - NY / NY State')0
