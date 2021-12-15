e72caf6e29570a1016704aebeada537411334165
Sophia Peraza <sophiasperaza@gmail.com>
2021-12-14 19:47:46 -0500

    had to fix syntax

--- Streamlit_attempt.py
+++ Streamlit_attempt.py
@@ -13,6 +13,6 @@ import numpy as np
 
 st.title('CMS - Hospital Data ')
 
-df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv'
+df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
 
 st.dataframe(df)               
