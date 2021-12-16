eabbfb09eb6cd8c0a56cbe0c477ca727ca470f12
Sophia Peraza <sophiasperaza@gmail.com>
2021-12-16 13:22:53 -0500

    revert back

--- Streamlit_attempt.py
+++ Streamlit_attempt.py
@@ -43,11 +43,11 @@ st.title('Medicare — Expenses - NY / NY State')
 
     
     
-FAKE LOADER BAR TO STIMULATE LOADING    
-my_bar = st.progress(0)
-for percent_complete in range(100):
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1)
+# FAKE LOADER BAR TO STIMULATE LOADING    
+# my_bar = st.progress(0)
+# for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)
   
 
 st.title('HHA 507 - Final Assignment')
